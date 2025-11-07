308adb14225dfdd0308e884f1b024f33f8b2be8a
christiantoDavid <david.christianto05@gmail.com>
2025-11-07 18:12:14 +0800

    [db problem1]

--- invoiceManagement/settings.py
+++ invoiceManagement/settings.py
@@ -105,7 +105,7 @@ WSGI_APPLICATION = 'invoiceManagement.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
         conn_health_checks=True,
     )
