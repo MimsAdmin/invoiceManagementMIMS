282f3e84bf9def4de45f0255b37e850ffb4abbce
christiantoDavid <david.christianto05@gmail.com>
2025-11-09 22:20:12 +0800

    updated deployment1

--- invoiceManagement/settings.py
+++ invoiceManagement/settings.py
@@ -159,8 +159,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"] if (BASE_DIR / "static").exists() else []
 
 
 # Default primary key field type
