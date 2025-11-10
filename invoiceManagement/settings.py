cf485327ef8014d55c453d0667f4474fd7808f3f
christiantoDavid <david.christianto05@gmail.com>
2025-11-10 16:06:31 +0800

    pliss rampung semua nya dong

--- invoiceManagement/settings.py
+++ invoiceManagement/settings.py
@@ -135,7 +135,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"] if (BASE_DIR / "static").exists() else []
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
